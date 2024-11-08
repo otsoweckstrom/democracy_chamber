60b6c3f8642a209a6ede5b9b074269337e5d04dd
otsoweckstrom <97otso@gmail.com>
2024-11-09 00:04:02 +0200

    update front

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [message, setMessage] = useState('')
+
+	useEffect(() => {
+		// Fetch data from your Node.js backend
+		fetch(`${process.env.REACT_APP_API_URL}/api`)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok')
+				}
+				return response.json()
+			})
+			.then((data) => setMessage(data.message))
+			.catch((err) => console.error('Error fetching data:', err))
+	}, [])
+
+	return (
+		<div className="App">
+			<h1>React Frontend Connected to Node.js Backend</h1>
+			<p>{message}</p>
+		</div>
+	)
 }
 
-export default App;
+export default App
