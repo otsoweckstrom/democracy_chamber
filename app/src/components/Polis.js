f32c74f20d52155ffcb0cc664c5311b3d36322da
Tukkimies Jonni E <jonnituk@cubbli22-gold.local>
2024-11-09 12:07:52 +0200

    Added new attributes for Polis elements

--- app/src/components/Polis.js
+++ app/src/components/Polis.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import '../styles/polis.css'
+import React, { useEffect } from 'react';
 
-const Polis = () => {
-	return (
-		<div className="poll-container">
-			<iframe
-				src="https://pol.is/4in4nrryzd"
-				width="100%"
-				height="1000px"
-				frameBorder="0"
-				allowFullScreen
-				title="Pol.is Conversation"
-			></iframe>
-			<iframe
-				src="https://pol.is/53mrmhyadj"
-				width="100%"
-				height="1000px"
-				frameBorder="0"
-				allowFullScreen
-				title="Pol.is Conversation"
-			></iframe>
-		</div>
-	)
-}
+// Embed Pol.is poll using a dynamic conversationId and height/width customization
+const Polis = ({ conversationId, height = "600px", width = "100%" }) => {
+  useEffect(() => {
+    // Dynamically load Pol.is embed script when the component is mounted
+    const script = document.createElement('script');
+    script.src = 'https://pol.is/embed.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }, []); // Run once on mount
 
-export default Polis
+  return (
+    <div className="polis-embed-container" style={{ width, height }}>
+      <div
+        className="polis"
+		    data-ucv="true"
+		    data-ucw="false"
+		    data-ucsf="true"
+		    data-xid="true"
+		    data-x_profile_image_url="true"
+		    data-build="false"
+        data-conversation_id={conversationId} // Use the dynamic conversation ID
+        style={{ width: '30%', height: '30%' }}
+      />
+    </div>
+  );
+};
+
+export default Polis;
+
