ff73b25d5442f660ab77468d591afb0940bb456f
otsoweckstrom <97otso@gmail.com>
2024-11-09 17:59:07 +0200

    create addPollPage

--- app/src/App.js
+++ app/src/App.js
@@ -1,64 +1,62 @@
-import React, { useEffect, useState } from "react";
-import Polis from "./components/Polis";
-import LoginForm from "./components/LoginForm";
-import LogoutForm from "./components/LogoutForm";
-import AddPolisTopic from "./components/AddPolisTopic";
-import Threads from "./components/Threads";
+import React, { useEffect, useState } from 'react'
+import Polis from './components/Polis'
+import LoginForm from './components/LoginForm'
+import LogoutForm from './components/LogoutForm'
+import Threads from './components/Threads'
+import AddPollPage from './components/AddPollPage'
 
 function App() {
-  const [polisThreads, setPolisThreads] = useState([]);
-  const [loggedInUser, setLoggedInUser] = useState(null);
-  const threadsData = [
-    {
-      id: 1,
-      title: "First Thread",
-      content: "This is the content of the first thread.",
-    },
-    {
-      id: 2,
-      title: "Second Thread",
-      content: "This is the content of the second thread.",
-    },
-    {
-      id: 3,
-      title: "Third Thread",
-      content: "This is the content of the third thread.",
-    },
-  ];
+	const [polisThreads, setPolisThreads] = useState([])
+	const [loggedInUser, setLoggedInUser] = useState(null)
+	const threadsData = [
+		{
+			id: 1,
+			title: 'First Thread',
+			content: 'This is the content of the first thread.',
+		},
+		{
+			id: 2,
+			title: 'Second Thread',
+			content: 'This is the content of the second thread.',
+		},
+		{
+			id: 3,
+			title: 'Third Thread',
+			content: 'This is the content of the third thread.',
+		},
+	]
 
-  useEffect(() => {
-    // Fetch data from your Node.js backend.
-    fetch(`${process.env.REACT_APP_API_URL}/api/links`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => setPolisThreads(data))
-      .catch((err) => console.error("Error fetching data:", err));
-  }, []);
+	useEffect(() => {
+		fetch(`${process.env.REACT_APP_API_URL}/api/links`)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok')
+				}
+				return response.json()
+			})
+			.then((data) => setPolisThreads(data))
+			.catch((err) => console.error('Error fetching data:', err))
+	}, [])
 
-  return (
-    <div className="App">
-      <div className="Threads test">
-        <Threads threads={threadsData} />
-      </div>
-      <div className="login-logout">
-        {loggedInUser ? (
-          <LogoutForm
-            onLogout={() => setLoggedInUser(null)}
-            loggedInUser={loggedInUser}
-          />
-        ) : (
-          <LoginForm onLoginSuccess={setLoggedInUser} /> // Pass setter function
-        )}
-      </div>
-
-      <AddPolisTopic />
-      <Polis />
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="Threads test">
+				<Threads threads={threadsData} />
+			</div>
+			<div className="login-logout">
+				{loggedInUser ? (
+					<LogoutForm
+						onLogout={() => setLoggedInUser(null)}
+						loggedInUser={loggedInUser}
+					/>
+				) : (
+					<LoginForm onLoginSuccess={setLoggedInUser} />
+				)}
+			</div>
+			<AddPollPage />
+			<Polis />
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ app/src/components/AddPollPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddPolisTopic from './AddPolisTopic'
+
+const AddPollPage = () => {
+	return (
+		<div>
+			<h2>Add Poll</h2>
+			<AddPolisTopic />
+		</div>
+	)
+}
+
+export default AddPollPage
